33755166852f282c2509dc62349053d3402b720b
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-03-13 16:13:14 +0100

    Implement get and insert in Style

--- src/Style.cpp
+++ src/Style.cpp
@@ -6,7 +6,13 @@
 
 #include "Style.h"
 
+#include <QUrl>
+
+#include "ElementIdentifier.h"
 #include "StyleLoader.h"
+#include "input/plasmasvg/PlasmaSvgLoader.h"
+
+#include "union_logging.h"
 
 using namespace Union;
 
@@ -14,21 +20,40 @@ class UNION_NO_EXPORT Style::Private
 {
 public:
     std::unique_ptr<StyleLoader> styleLoader;
+
+    std::unordered_map<ElementIdentifier, std::shared_ptr<StyleElement>> elements;
 };
 
 Style::Style()
     : QObject(nullptr)
     , d(std::make_unique<Private>())
 {
-
 }
 
 Style::~Style() = default;
 
-std::shared_ptr<StyleElement> Style::get(const ElementSelector &selector)
+std::shared_ptr<StyleElement> Style::get(const ElementIdentifier &selector)
 {
-//     return d->styleLoader->get(selector);
+    if (!d->styleLoader) {
+        d->styleLoader = std::make_unique<PlasmaSvgLoader>(instance());
+        if (!d->styleLoader->load()) {
+            qCCritical(UNION_GENERAL) << "Failed to load input!";
+            return nullptr;
+        }
+    }
+
+    auto element = d->elements.find(selector);
+    if (element != d->elements.end()) {
+        return element->second;
+    }
+
     return nullptr;
+}
+
+void Style::insert(const ElementIdentifier &identifier, std::shared_ptr<StyleElement> element)
+{
+    auto pair = std::make_pair(identifier, element);
+    d->elements.insert(pair);
 }
 
 std::shared_ptr<Style> Style::instance()
