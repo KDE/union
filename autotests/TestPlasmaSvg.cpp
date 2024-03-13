4a14fa20574770e661b1d842c7a019e6ecc24a4e
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-03-13 16:09:22 +0100

    Add a very basic autotest for PlasmaSvg input

---
+++ autotests/TestPlasmaSvg.cpp
@@ -0,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Arjen Hiemstra <ahiemstra@heimr.nl>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+ */
+
+#include <QtTest>
+
+#include <ElementIdentifier.h>
+#include <Style.h>
+
+using namespace Union;
+
+class TestPlasmaSvg : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void testLoad()
+    {
+        auto normalButtonId = ElementIdentifier{QStringLiteral("plasma-svg"), QStringLiteral("button"), QStringLiteral("normal")};
+        auto element = Style::instance()->get(normalButtonId);
+
+        QVERIFY(element);
+    }
+};
+
+QTEST_MAIN(TestPlasmaSvg)
+
+#include "TestPlasmaSvg.moc"
