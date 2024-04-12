670a1a514b739756fc4ede0f783f9dbf99b4d698
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-04-12 14:26:10 +0200

    Add an autotest for Style
    Tests the three methods with actual logic

---
+++ autotests/TestStyle.cpp
@@ -0,0 +1,88 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Arjen Hiemstra <ahiemstra@heimr.nl>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+ */
+
+#include <QtTest>
+
+#include <Style.h>
+
+using namespace Union;
+
+class TestStyle : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void testContentSize()
+    {
+        auto style = Style::create();
+
+        QCOMPARE(style->contentSize(), QSizeF());
+
+        AreaDefinition background;
+        background.size = QSizeF(10, 10);
+        style->setBackground(background);
+
+        QCOMPARE(style->contentSize(), QSizeF(10, 10));
+
+        AreaDefinition foreground;
+        foreground.size = QSizeF(15, 15);
+        style->setForeground(foreground);
+
+        QCOMPARE(style->contentSize(), QSizeF(15, 15));
+
+        background.size = QSizeF(20, 20);
+        style->setBackground(background);
+
+        QCOMPARE(style->contentSize(), QSizeF(20, 20));
+    }
+
+    void testBorderSizes()
+    {
+        auto style = Style::create();
+
+        QCOMPARE(style->borderSizes(), QMarginsF());
+
+        BorderDefinition border;
+        LineDefinition line;
+        line.size = 10;
+        border.left = line;
+        border.top = line;
+        style->setBorder(border);
+
+        QCOMPARE(style->borderSizes(), QMarginsF(10, 10, 0, 0));
+
+        border.right = line;
+        border.bottom = line;
+        style->setBorder(border);
+
+        QCOMPARE(style->borderSizes(), QMarginsF(10, 10, 10, 10));
+    }
+
+    void testBoundingRect()
+    {
+        auto style = Style::create();
+
+        QCOMPARE(style->boundingRect(), QRectF());
+
+        AreaDefinition background;
+        background.size = QSizeF(10, 10);
+        style->setBackground(background);
+
+        QCOMPARE(style->boundingRect(), QRectF(0, 0, 10, 10));
+
+        BorderDefinition border;
+        LineDefinition line;
+        line.size = 10;
+        border.left = line;
+        border.top = line;
+        style->setBorder(border);
+
+        QCOMPARE(style->boundingRect(), QRectF(0, 0, 20, 20));
+    }
+};
+
+QTEST_MAIN(TestStyle)
+
+#include "TestStyle.moc"
