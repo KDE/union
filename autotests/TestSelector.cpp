52325e9f0901043e580dc0fccb861cd0d1b3074e
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-04-25 16:13:41 +0200

    autotests: Add a test to verify that empty selectors match nothing

--- autotests/TestSelector.cpp
+++ autotests/TestSelector.cpp
@@ -15,6 +15,30 @@ class TestSelector : public QObject
 {
     Q_OBJECT
 private Q_SLOTS:
+    void testEmptySelectors()
+    {
+        auto emptyElement = Element::create();
+
+        Selector emptySelector = Selector::create();
+        QVERIFY2(!emptySelector.matches(emptyElement), "Empty selector should not match an empty element");
+
+        emptySelector = Selector::create<SelectorType::Type>(QString{});
+        QVERIFY2(!emptySelector.matches(emptyElement), "Empty Type selector should not match an empty element");
+
+        emptySelector = Selector::create<SelectorType::Id>(QString{});
+        QVERIFY(!emptySelector.matches(emptyElement));
+        QVERIFY2(!emptySelector.matches(emptyElement), "Empty ID selector should not match an empty element");
+
+        emptySelector = Selector::create<SelectorType::State>(Element::State::None);
+        QVERIFY2(!emptySelector.matches(emptyElement), "Empty State selector should not match an empty element");
+
+        emptySelector = Selector::create<SelectorType::Hint>(QString{});
+        QVERIFY2(!emptySelector.matches(emptyElement), "Empty Hint selector should not match an empty element");
+
+        emptySelector = Selector::create<SelectorType::Attribute>(std::make_pair(QString{}, QVariant{}));
+        QVERIFY2(!emptySelector.matches(emptyElement), "Empty Attribute selector should not match an empty element");
+    }
+
     void testSimpleStructureMatches_data()
     {
         QTest::addColumn<SelectorList>("selectors");
