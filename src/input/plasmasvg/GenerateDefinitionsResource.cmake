a86c8773720064abb6f8cae1f233545bf3912045
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-07-18 16:27:53 +0200

    input/plasmasvg: Generate definitions QRC file from directory contents
    Rather than manually having to maintain it which is needless work and
    creates needless merge conflicts

---
+++ src/input/plasmasvg/GenerateDefinitionsResource.cmake
@@ -0,0 +1,10 @@
+# This generates a QRC file for the definitions by listing all yml files in the definitions directory.
+# This avoids having to maintain our own QRC file and related merge conflicts.
+file(GLOB _definitions definitions/*.yml)
+set(_qrc_content "<RCC>\n<qresource prefix=\"/org/kde/union/input/plasmasvg\">\n")
+foreach(_file ${_definitions})
+    get_filename_component(_name "${_file}" NAME)
+    string(APPEND _qrc_content "<file alias=\"${_name}\">${_file}</file>\n")
+endforeach()
+string(APPEND _qrc_content "</qresource>\n</RCC>\n")
+file(WRITE "${OUTPUT_DIR}/PlasmaSvgDefinitions.qrc" "${_qrc_content}")
