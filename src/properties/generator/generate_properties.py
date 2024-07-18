4f049470d7e2b42a273cb7f8f1b0e7aa2214b67d
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-07-18 17:53:44 +0200

    Add a script that generates property classes from yaml and templates
    The property classes are very similar. Defining them all manually is a
    lot of work and maintainance. However, with some clever python scripting
    we can instead generate them using a structure defined in a YAML file.
    This greatly reduces the amount of duplicate code we need to write,
    while also giving an improved overview of what the actual data structure
    looks like. We can even generate autotests for all the property classes.

---
+++ src/properties/generator/generate_properties.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+import dataclasses
+from pathlib import Path
+
+import yaml
+import jinja2
+
+base_directory = Path(__file__).parent
+
+
+include_patterns = [
+    {"pattern": "Qt::", "use_include": "QtGlobal", "system_include": True},
+    {"pattern": "qreal", "use_include": None},
+    {"pattern": "Q", "system_include": True},
+    {"pattern": "Union::Properties::", "use_include": "Types.h"},
+    {"pattern": "", },
+]
+
+
+class AliasNode(yaml.Node):
+    id = "anchor"
+
+
+class PreserveAliasesLoader(yaml.SafeLoader):
+    def __init__(self, stream):
+        super().__init__(stream)
+
+    def compose_node(self, parent, index):
+        if self.check_event(yaml.AliasEvent):
+            event = self.get_event()
+            return AliasNode("", event.anchor, event.start_mark, event.end_mark)
+
+        event = self.peek_event()
+        result = super().compose_node(parent, index)
+
+        if event.anchor is not None:
+            result.anchor = event.anchor
+        else:
+            result.anchor = None
+
+        return result
+
+
+@dataclasses.dataclass
+class ValueDescription:
+    name: str
+    type: str = ""
+    is_property: bool = False
+
+
+@dataclasses.dataclass
+class PropertyDescription:
+    type_name: str
+    values: list[ValueDescription] = dataclasses.field(default_factory=list)
+    system_includes: set[str] = dataclasses.field(default_factory=set)
+    local_includes: set[str] = dataclasses.field(default_factory=set)
+
+    def __lt__(self, other):
+        return self.type_name < other.type_name
+
+
+def ucfirst(value):
+    return f"{value[0].upper()}{value[1:]}"
+
+
+def qualified_name(type_name):
+    return f"{ucfirst(type_name)}Property"
+
+
+def process_node(node, type_name, memo):
+    if not isinstance(node, yaml.MappingNode):
+        raise RuntimeError(f"Node {node} is not a mapping node!")
+
+    description = PropertyDescription(type_name)
+    memo[type_name] = description
+
+    for key_node, value_node in node.value:
+        value = ValueDescription(key_node.value)
+        match type(value_node).__name__:
+            case "MappingNode":
+                type_name = qualified_name(value_node.anchor) if value_node.anchor is not None else qualified_name(key_node.value)
+                memo = memo | process_node(value_node, type_name, memo)
+
+                value.type = type_name
+                value.is_property = True
+
+            case "AliasNode":
+                sub_property = memo[qualified_name(value_node.value)]
+
+                value.type = sub_property.type_name
+                value.is_property = True
+
+            case "ScalarNode":
+                value.type = value_node.value
+
+        for entry in include_patterns:
+            if not value.type.startswith(entry["pattern"]):
+                continue
+
+            system = entry.get("system_include", False)
+            include = entry.get("use_include", value.type if system else value.type + ".h")
+
+            if include is None:
+                break
+
+            if system:
+                description.system_includes.add(include)
+            else:
+                description.local_includes.add(include)
+
+            break
+
+        description.values.append(value)
+
+    return memo
+
+
+if __name__ == "__main__":
+    structure = None
+    with open(base_directory / "properties.yml") as f:
+        structure = yaml.compose(f, PreserveAliasesLoader)
+
+    types = process_node(structure, "StyleProperty", {})
+
+    jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(base_directory),
+        autoescape=False,
+        trim_blocks=True,
+    )
+    jinja_env.filters["ucfirst"] = ucfirst
+
+    for type_name, type_definition in types.items():
+        data = dataclasses.asdict(type_definition)
+
+        with open((base_directory.parent / type_name).with_suffix(".h"), "w") as f:
+            template = jinja_env.get_template("property.h.j2")
+            f.write(template.render(data))
+
+        with open((base_directory.parent / type_name).with_suffix(".cpp"), "w") as f:
+            template = jinja_env.get_template("property.cpp.j2")
+            f.write(template.render(data))
+
+        with open((base_directory.parent / "autotests" / ("Test" + type_name)).with_suffix(".cpp"), "w") as f:
+            template = jinja_env.get_template("autotest.cpp.j2")
+            f.write(template.render(data))
+
+    with open(base_directory.parent / "autotests" / "CreateTestInstances.h", "w") as f:
+        template = jinja_env.get_template("CreateTestInstances.h.j2")
+        f.write(template.render({"types": types.values()}))
