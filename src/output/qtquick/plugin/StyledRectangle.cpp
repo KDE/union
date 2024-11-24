43a5ff32b8aac4f95702fd5e40917cad8388a998
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-11-24 18:06:02 +0100

    output/quick: Don't render anything if StyledRectangle's elements don't fit
    if the combined size of border and corners ends up bigger than
    StyledRectangle's size we end up overlapping corners which looks pretty
    bad. So in that case, just render nothing. The other option would be to
    render out of bounds but that is worse.

--- src/output/qtquick/plugin/StyledRectangle.cpp
+++ src/output/qtquick/plugin/StyledRectangle.cpp
@@ -41,6 +41,14 @@ bool StyledRectangle::event(QEvent *event)
 
 QSGNode *StyledRectangle::updatePaintNode(QSGNode *node, QQuickItem::UpdatePaintNodeData * /*data*/)
 {
+    // Ensure we clean up the old node if conditions change so we no longer want
+    // to render anything.
+    auto guard = qScopeGuard([node]() {
+        if (node) {
+            delete node;
+        }
+    });
+
     if (!m_style) {
         return nullptr;
     }
@@ -54,14 +62,27 @@ QSGNode *StyledRectangle::updatePaintNode(QSGNode *node, QQuickItem::UpdatePaint
         return nullptr;
     }
 
+    auto rect = boundingRect();
+    auto background = query->properties().background().value();
+    auto minSize = minimumBackgroundSize(background);
+
+    // If we cannot fit the background image into the required rect, just don't
+    // display anything.'
+    if (rect.width() < minSize.width() || rect.height() < minSize.height()) {
+        return nullptr;
+    }
+
     if (!node) {
         node = new RectangleNode{};
     }
 
     auto rectangleNode = static_cast<RectangleNode *>(node);
-    rectangleNode->setRectangle(query->properties().background().value());
-    rectangleNode->rect = boundingRect();
+    rectangleNode->setRectangle(background);
+    rectangleNode->rect = rect;
     rectangleNode->update(window());
+
+    // Ensure we *don't* delete the node at this point.
+    guard.dismiss();
 
     return rectangleNode;
 }
@@ -83,4 +104,37 @@ void StyledRectangle::updateImplicitSize()
     }
 
     update();
+}
+
+QSizeF StyledRectangle::minimumBackgroundSize(const Properties::BackgroundProperty &background)
+{
+    if (!background.corners() && !background.border()) {
+        return QSizeF{0.0, 0.0};
+    }
+
+    auto corners = background.corners().value_or(Properties::CornersProperty{});
+    auto border = background.border().value_or(Properties::BorderProperty{});
+
+    auto left = std::initializer_list<qreal>{
+        corners.topLeft().value_or(Properties::CornerProperty{}).width().value_or(0.0),
+        border.left().value_or(Properties::LineProperty{}).size().value_or(0.0),
+        corners.bottomLeft().value_or(Properties::CornerProperty{}).width().value_or(0.0),
+    };
+    auto right = std::initializer_list<qreal>{
+        corners.topRight().value_or(Properties::CornerProperty{}).width().value_or(0.0),
+        border.right().value_or(Properties::LineProperty{}).size().value_or(0.0),
+        corners.bottomRight().value_or(Properties::CornerProperty{}).width().value_or(0.0),
+    };
+    auto top = std::initializer_list<qreal>{
+        corners.topLeft().value_or(Properties::CornerProperty{}).height().value_or(0.0),
+        border.top().value_or(Properties::LineProperty{}).size().value_or(0.0),
+        corners.topRight().value_or(Properties::CornerProperty{}).height().value_or(0.0),
+    };
+    auto bottom = std::initializer_list<qreal>{
+        corners.bottomLeft().value_or(Properties::CornerProperty{}).height().value_or(0.0),
+        border.bottom().value_or(Properties::LineProperty{}).size().value_or(0.0),
+        corners.bottomRight().value_or(Properties::CornerProperty{}).height().value_or(0.0),
+    };
+
+    return QSizeF{std::max(left) + std::max(right), std::max(top) + std::max(bottom)};
 }
