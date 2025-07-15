ea104d6203375ae9bf39fec6e086e6fad21d3cc3
Arjen Hiemstra <ahiemstra@heimr.nl>
2025-07-15 17:39:01 +0200

    output/kirigami: Add an initial Kirigami platform plugin
    Only implements Units right now as PlatformTheme requires extra work.

---
+++ src/output/kirigami/PluginFactory.cpp
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+// SPDX-FileCopyrightText: 2025 Arjen Hiemstra <ahiemstra@heimr.nl>
+
+#include "PluginFactory.h"
+
+#include "PlatformTheme.h"
+#include "Units.h"
+
+PluginFactory::PluginFactory(QObject *parent)
+    : Kirigami::Platform::PlatformPluginFactory(parent)
+{
+}
+
+Kirigami::Platform::Units *PluginFactory::createUnits(QObject *parent)
+{
+    return new Units(parent);
+}
