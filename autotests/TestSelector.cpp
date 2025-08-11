466e087f9f9824c8d584423468e8de5216b5c4d7
Arjen Hiemstra <ahiemstra@heimr.nl>
2025-08-11 14:05:05 +0200

    Selector: Use Element* for borrowed Element in matches()
    This removes the overhead of creating/destroying shared_ptr instances
    and more directly indicates the intent of this being a borrow.

--- autotests/TestSelector.cpp
+++ autotests/TestSelector.cpp
@@ -261,29 +261,29 @@ private Q_SLOTS:
         auto emptyElement = Element::create();
 
         Selector emptySelector = Selector::create();
-        QVERIFY2(!emptySelector.matches(emptyElement), "Empty selector should not match an empty element");
+        QVERIFY2(!emptySelector.matches(emptyElement.get()), "Empty selector should not match an empty element");
 
         emptySelector = Selector::create<SelectorType::Type>(QString{});
-        QVERIFY2(!emptySelector.matches(emptyElement), "Empty Type selector should not match an empty element");
+        QVERIFY2(!emptySelector.matches(emptyElement.get()), "Empty Type selector should not match an empty element");
 
         emptySelector = Selector::create<SelectorType::Id>(QString{});
-        QVERIFY(!emptySelector.matches(emptyElement));
-        QVERIFY2(!emptySelector.matches(emptyElement), "Empty ID selector should not match an empty element");
+        QVERIFY(!emptySelector.matches(emptyElement.get()));
+        QVERIFY2(!emptySelector.matches(emptyElement.get()), "Empty ID selector should not match an empty element");
 
         emptySelector = Selector::create<SelectorType::State>(Element::State::None);
-        QVERIFY2(!emptySelector.matches(emptyElement), "Empty State selector should not match an empty element");
+        QVERIFY2(!emptySelector.matches(emptyElement.get()), "Empty State selector should not match an empty element");
 
         emptySelector = Selector::create<SelectorType::Hint>(QString{});
-        QVERIFY2(!emptySelector.matches(emptyElement), "Empty Hint selector should not match an empty element");
+        QVERIFY2(!emptySelector.matches(emptyElement.get()), "Empty Hint selector should not match an empty element");
 
         emptySelector = Selector::create<SelectorType::AttributeExists>(QString{});
-        QVERIFY2(!emptySelector.matches(emptyElement), "Empty Attribute Exists selector should not match an empty element");
+        QVERIFY2(!emptySelector.matches(emptyElement.get()), "Empty Attribute Exists selector should not match an empty element");
 
         emptySelector = Selector::create<SelectorType::AttributeEquals>(std::make_pair(QString{}, QVariant{}));
-        QVERIFY2(!emptySelector.matches(emptyElement), "Empty Attribute Equals selector should not match an empty element");
+        QVERIFY2(!emptySelector.matches(emptyElement.get()), "Empty Attribute Equals selector should not match an empty element");
 
         emptySelector = Selector::create<SelectorType::AttributeSubstringMatch>(std::make_pair(QString{}, QString{}));
-        QVERIFY2(!emptySelector.matches(emptyElement), "Empty Attribute Substring Match selector should not match an empty element");
+        QVERIFY2(!emptySelector.matches(emptyElement.get()), "Empty Attribute Substring Match selector should not match an empty element");
     }
 
     void testSelectors_data()
