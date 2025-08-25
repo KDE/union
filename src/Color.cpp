50c8e87b0d7ca8b6ffc9dd43bf5fbdae953f56a8
Arjen Hiemstra <ahiemstra@heimr.nl>
2025-08-25 18:12:54 +0200

    Introduce Color as a custom color class

---
+++ src/Color.cpp
@@ -0,0 +1,298 @@
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+// SPDX-FileCopyrightText: 2025 Arjen Hiemstra <ahiemstra@quantumproductions.info>
+
+#include "Color.h"
+
+#include <QSharedData>
+
+#include "union_logging.h"
+
+using namespace Union;
+using namespace Qt::StringLiterals;
+
+struct RgbaData : ColorData {
+    RgbaData(uint8_t _r, uint8_t _g, uint8_t _b, uint8_t _a)
+        : ColorData(Type::RGBA)
+        , r(_r)
+        , g(_g)
+        , b(_b)
+        , a(_a)
+    {
+    }
+
+    RgbaData(const RgbaData &other)
+        : ColorData(other)
+    {
+        r = other.r;
+        g = other.g;
+        b = other.b;
+        a = other.a;
+    }
+
+    inline ColorData *toRgba() const override
+    {
+        return new RgbaData(r, g, b, a);
+    }
+
+    inline QString toString() const override
+    {
+        return u"RGBA, %1, %2, %3, %4"_s.arg(r).arg(g).arg(b).arg(a);
+    }
+
+    inline bool equals(const ColorData *other) const override
+    {
+        auto otherRgba = static_cast<const RgbaData *>(other);
+        return r == otherRgba->r && g == otherRgba->g && b == otherRgba->b && a == otherRgba->a;
+    }
+
+    uint8_t r = 0;
+    uint8_t g = 0;
+    uint8_t b = 0;
+    uint8_t a = 0;
+};
+
+struct CustomData : ColorData {
+    CustomData(const QString &_source, const QStringList &_arguments)
+        : ColorData(Type::Custom)
+        , source(_source)
+        , arguments(_arguments)
+    {
+    }
+
+    CustomData(const CustomData &other)
+        : ColorData(other)
+    {
+        source = other.source;
+        arguments = other.arguments;
+    }
+
+    inline ColorData *toRgba() const override
+    {
+        auto provider = ColorProvider::provider(source);
+        if (!provider) {
+            qCWarning(UNION_GENERAL) << "No provider" << source << "found";
+            return nullptr;
+        }
+
+        auto colorValue = provider->color(arguments);
+        if (colorValue) {
+            auto rgba = colorValue.value();
+            return new RgbaData(rgba.r, rgba.g, rgba.b, rgba.a);
+        }
+
+        return nullptr;
+    }
+
+    inline QString toString() const override
+    {
+        return u"Custom, source: %1, arguments: %2"_s.arg(source, arguments.join(u", "));
+    }
+
+    inline bool equals(const ColorData *other) const override
+    {
+        auto otherCustom = static_cast<const CustomData *>(other);
+        return source == otherCustom->source && arguments == otherCustom->arguments;
+    }
+
+    QString source;
+    QStringList arguments;
+};
+
+struct MixData : ColorData {
+    MixData(const Color &_first, const Color &_second, qreal _amount)
+        : ColorData(Type::Mix)
+        , first(_first)
+        , second(_second)
+        , amount(_amount)
+    {
+    }
+
+    MixData(const MixData &other)
+        : ColorData(other)
+    {
+        first = other.first;
+        second = other.second;
+        amount = other.amount;
+    }
+
+    inline ColorData *toRgba() const override
+    {
+        auto firstRgba = first.toRgba();
+        auto secondRgba = second.toRgba();
+
+        auto invAmount = 1.0 - amount;
+
+        return new RgbaData{
+            uint8_t(firstRgba.red() * invAmount + secondRgba.red() * amount),
+            uint8_t(firstRgba.green() * invAmount + secondRgba.green() * amount),
+            uint8_t(firstRgba.blue() * invAmount + secondRgba.blue() * amount),
+            uint8_t(firstRgba.alpha() * invAmount + secondRgba.alpha() * amount),
+        };
+    }
+
+    inline QString toString() const override
+    {
+        return u"Mix, first: %1, second: %2, amount: %3"_s.arg(first.toString(), second.toString()).arg(amount);
+    }
+
+    inline bool equals(const ColorData *other) const override
+    {
+        auto otherMix = static_cast<const MixData *>(other);
+        return first == otherMix->first && second == otherMix->second && qFuzzyCompare(amount, otherMix->amount);
+    }
+
+    Color first;
+    Color second;
+    qreal amount;
+};
+
+Color::Color()
+{
+}
+
+Color::Color(ColorData *_data)
+    : data(_data)
+{
+}
+
+Color::Color(const Color &other)
+{
+    *this = other;
+}
+
+Color &Color::operator=(const Color &other)
+{
+    data = other.data;
+    return *this;
+}
+
+bool Color::operator==(const Color &other) const
+{
+    if (data == other.data) {
+        return true;
+    }
+
+    if (data->type != other.data->type) {
+        return false;
+    }
+
+    return data->equals(other.data.get());
+}
+
+bool Color::isValid() const
+{
+    return bool(data);
+}
+
+ColorData::Type Color::type() const
+{
+    return data->type;
+}
+
+uint8_t Color::red() const
+{
+    if (data->type == ColorData::Type::RGBA) {
+        return static_cast<const RgbaData *>(data.get())->r;
+    }
+
+    return 0;
+}
+
+uint8_t Color::green() const
+{
+    if (data->type == ColorData::Type::RGBA) {
+        return static_cast<const RgbaData *>(data.get())->g;
+    }
+
+    return 0;
+}
+
+uint8_t Color::blue() const
+{
+    if (data->type == ColorData::Type::RGBA) {
+        return static_cast<const RgbaData *>(data.get())->b;
+    }
+
+    return 0;
+}
+
+uint8_t Color::alpha() const
+{
+    if (data->type == ColorData::Type::RGBA) {
+        return static_cast<const RgbaData *>(data.get())->a;
+    }
+
+    return 0;
+}
+
+Color Color::toRgba() const
+{
+    if (!data) {
+        return Color{};
+    }
+
+    if (data->type == ColorData::Type::RGBA) {
+        return *this;
+    }
+
+    return Color{data->toRgba()};
+}
+
+QColor Color::toQColor() const
+{
+    const Color rgba = toRgba();
+    if (!rgba.isValid()) {
+        return Qt::transparent;
+    }
+
+    auto rgbaData = static_cast<const RgbaData *>(rgba.data.get());
+    return QColor(rgbaData->r, rgbaData->g, rgbaData->b, rgbaData->a);
+}
+
+QString Color::toString() const
+{
+    return u"Color("_s + data->toString() + u")"_s;
+}
+
+Color Color::rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+    return Color(new RgbaData(r, g, b, a));
+}
+
+Color Color::custom(const QString &source, const QStringList &arguments)
+{
+    return Color(new CustomData(source, arguments));
+}
+
+Color Color::mix(const Color &first, const Color &second, qreal amount)
+{
+    return Color(new MixData(first, second, amount));
+}
+
+ColorProvider::ColorProvider()
+{
+}
+
+ColorProvider::~ColorProvider() noexcept
+{
+}
+
+std::shared_ptr<ColorProvider> ColorProvider::provider(const QString &source)
+{
+    if (s_providers.contains(source)) {
+        return s_providers.value(source);
+    }
+
+    return nullptr;
+}
+
+void Union::ColorProvider::registerProvider(const QString &source, const std::shared_ptr<ColorProvider> &provider)
+{
+    s_providers[source] = provider;
+}
+
+QDebug &operator<<(QDebug &stream, const Union::Color &color)
+{
+    stream << color.toString();
+    return stream;
+}
---
+++ src/Color.h
@@ -0,0 +1,204 @@
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+// SPDX-FileCopyrightText: 2025 Arjen Hiemstra <ahiemstra@quantumproductions.info>
+
+#pragma once
+
+#include <memory>
+
+#include <QColor>
+#include <QSharedData>
+#include <QString>
+#include <QVariant>
+
+#include "union_export.h"
+
+namespace Union
+{
+
+// Private data for Color
+struct ColorData : public QSharedData {
+    enum class Type {
+        Empty,
+        RGBA,
+        Custom,
+        Mix,
+    };
+
+    ColorData(Type _type)
+        : QSharedData()
+        , type(_type)
+    {
+    }
+
+    ColorData(const ColorData &other)
+        : QSharedData(other)
+        , type(other.type)
+    {
+    }
+
+    virtual ~ColorData()
+    {
+    }
+
+    virtual ColorData *toRgba() const = 0;
+    virtual QString toString() const = 0;
+    virtual bool equals(const ColorData *other) const = 0;
+
+    const Type type = Type::Empty;
+};
+
+/*!
+ * \class Union::Color
+ * \inmodule core
+ * \ingroup core-classes
+ *
+ * \brief A class that represents a color that can be provided from different sources.
+ *
+ * The main purpose of this class is to store information about a color without
+ * needing to resolve said color to an actual concrete RGBA value. This allows
+ * input plugins to provide colors that can be cached without having to
+ * regenerate the cache if some color changes.
+ */
+class UNION_EXPORT Color
+{
+public:
+    /*!
+     * Constructor.
+     *
+     * Constructs an invalid color.
+     */
+    Color();
+
+    Color(const Color &other);
+
+    Color &operator=(const Color &other);
+
+    bool operator==(const Color &other) const;
+
+    /*!
+     * Returns if this color has any data.
+     */
+    bool isValid() const;
+
+    /*!
+     * Returns the type of color.
+     */
+    ColorData::Type type() const;
+
+    /*!
+     * Returns the red component of this color if this is an RGBA color, otherwise returns 0.
+     */
+    uint8_t red() const;
+    /*!
+     * Returns the green component of this color if this is an RGBA color, otherwise returns 0.
+     */
+    uint8_t green() const;
+    /*!
+     * Returns the blue component of this color if this is an RGBA color, otherwise returns 0.
+     */
+    uint8_t blue() const;
+    /*!
+     * Returns the alpha component of this color if this is an RGBA color, otherwise returns 0.
+     */
+    uint8_t alpha() const;
+
+    /*!
+     * Returns this color converted to an RGBA color.
+     *
+     * How the exact conversion happens depends on the current color type.
+     */
+    Color toRgba() const;
+
+    /*!
+     * Returns this color converted to a QColor.
+     *
+     * If this color is not of RGBA type, it is first converted to RGBA before
+     * converting to QColor.
+     */
+    QColor toQColor() const;
+
+    /*!
+     * Returns a string representation of this color.
+     */
+    QString toString() const;
+
+    /*!
+     * Return a new RGBA color.
+     */
+    static Color rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255);
+    /*!
+     * Return a new custom color.
+     *
+     * \p source is the custom color source, which needs to be the name of a
+     * ColorProvider. \p arguments is a list of arguments to supply to the
+     * specified ColorProvider.
+     */
+    static Color custom(const QString &source, const QStringList &arguments);
+    /*!
+     * Return a new mix color.
+     *
+     * Mix colors represent the linear interpolation between the colors \p first
+     * and \p second with the specified \p amount.
+     */
+    static Color mix(const Color &first, const Color &second, qreal amount);
+
+private:
+    Color(ColorData *_data);
+
+    QSharedDataPointer<ColorData> data;
+};
+
+/*!
+ * \class ColorProvider
+ * \inmodule core
+ * \ingroup core-classes
+ *
+ * \brief An interface for objects that can provide custom colors.
+ */
+class UNION_EXPORT ColorProvider
+{
+public:
+    struct Rgba {
+        uint8_t r = 0;
+        uint8_t g = 0;
+        uint8_t b = 0;
+        uint8_t a = 0;
+    };
+
+    ColorProvider();
+    virtual ~ColorProvider();
+
+    /*!
+     * Returns a color matching the provided arguments.
+     *
+     * The arguments depend on the actual provider implementation.
+     */
+    virtual std::optional<Rgba> color(const QStringList &arguments) const = 0;
+
+    static void registerProvider(const QString &source, const std::shared_ptr<ColorProvider> &provider);
+    static std::shared_ptr<ColorProvider> provider(const QString &source);
+
+private:
+    inline static QHash<QString, std::shared_ptr<ColorProvider>> s_providers;
+};
+
+}
+
+UNION_EXPORT QDebug &operator<<(QDebug &stream, const Union::Color &color);
+
+/*!
+ * \relates Union::Color
+ *
+ * Specialization of std::formatter to support formatting an element as string.
+ */
+template<>
+struct std::formatter<Union::Color, char> : public std::formatter<std::string, char> {
+    template<class FormatContext>
+    FormatContext::iterator format(const Union::Color &value, FormatContext &context) const
+    {
+        auto string = value.toString().toStdString();
+        return std::formatter<std::string, char>::format(string, context);
+    }
+};
+
+Q_DECLARE_METATYPE(Union::Color)
