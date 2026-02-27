2c98154e6735ae496886ffb1a741cf8f837f00c6
Arjen Hiemstra <ahiemstra@heimr.nl>
2026-02-27 15:45:41 +0100

    Introduce PlaformPlugin as a base class for platform plugins
    This will contain any logic that is platform specific or requires
    platform dependencies.

---
+++ src/PlatformPlugin.cpp
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+// SPDX-FileCopyrightText: 2026 Arjen Hiemstra <ahiemstra@heimr.nl>
+
+#include "PlatformPlugin.h"
+
+#include <QIcon>
+
+using namespace Union;
+using namespace Qt::StringLiterals;
+
+PlatformPlugin::PlatformPlugin(QObject *parent)
+    : QObject(parent)
+{
+}
+
+QString PlatformPlugin::defaultInputPlugin()
+{
+    return QString{};
+}
+
+QIcon PlatformPlugin::platformIcon(const QString &name, [[maybe_unused]] const QColor &color)
+{
+    return QIcon::fromTheme(name);
+}
