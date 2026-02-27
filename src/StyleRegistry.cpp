50a318e11fad981b8d3273f513f2b430c7bb5f47
Arjen Hiemstra <ahiemstra@heimr.nl>
2026-02-27 15:51:42 +0100

    Implement loading platform plugins in StyleRegistry
    Try and find a plugin that matches with Qt's platformName and the
    current XDG_SESSION_DESKTOP. If we can't find anything, fallback to a
    mostly dummy platform.

--- src/StyleRegistry.cpp
+++ src/StyleRegistry.cpp
@@ -5,11 +5,11 @@
 
 #include <filesystem>
 
-#include <QCoreApplication>
+#include <QGuiApplication>
+#include <QJsonArray>
 #include <QPluginLoader>
 #include <QThread>
 
-#include "InputPlugin.h"
 #include "PluginRegistry.h"
 #include "Style.h"
 
@@ -18,11 +18,27 @@
 using namespace Union;
 using namespace Qt::StringLiterals;
 
+// This is implements the most minimal of PlatformPlugin that can be used as a
+// fallback if we have no existing platform plugin for the current platform.
+class FallbackPlatformPlugin : public PlatformPlugin
+{
+    QString defaultStyleName() override
+    {
+        return u"breeze"_s;
+    }
+
+    ColorProvider *createColorProvider([[maybe_unused]] const QString &providerName) override
+    {
+        return nullptr;
+    }
+};
+
 class Union::StyleRegistryPrivate
 {
 public:
     StyleRegistryPrivate()
         : inputRegistry{std::make_shared<PluginRegistry<InputPlugin>>(QJsonObject{{u"union-plugintype"_s, u"input"_s}})}
+        , platformRegistry{std::make_shared<PluginRegistry<PlatformPlugin>>(QJsonObject{{u"union-plugintype"_s, u"platform"_s}})}
     {
     }
 
@@ -59,8 +75,54 @@ public:
         return style;
     }
 
+    void loadPlatform()
+    {
+        const auto forcedPlatform = qEnvironmentVariable("UNION_FORCE_PLATFORM", QString{});
+        if (!forcedPlatform.isEmpty()) {
+            platform = std::shared_ptr<PlatformPlugin>(platformRegistry->pluginObject(forcedPlatform));
+            if (platform) {
+                return;
+            }
+        }
+
+        const auto platformName = QGuiApplication::platformName();
+        const auto sessionName = qEnvironmentVariable("XDG_SESSION_DESKTOP", QString{});
+
+        const auto plugins = platformRegistry->plugins();
+        for (const auto &plugin : plugins) {
+            const auto supportedPlatforms = plugin.metaData.value(u"union-supported-platforms").toArray().toVariantList();
+            auto platformItr = std::ranges::find_if(supportedPlatforms, [platformName](const QVariant &platform) {
+                return platformName.compare(platform.toString(), Qt::CaseInsensitive) == 0;
+            });
+            if (platformItr == supportedPlatforms.end()) {
+                continue;
+            }
+
+            const auto supportedSessions = plugin.metaData.value(u"union-supported-sessions").toArray().toVariantList();
+            if (!supportedSessions.isEmpty()) {
+                auto sessionItr = std::ranges::find_if(supportedSessions, [sessionName](const QVariant &session) {
+                    return sessionName.compare(session.toString(), Qt::CaseInsensitive) == 0;
+                });
+                if (sessionItr == supportedSessions.end()) {
+                    continue;
+                }
+            }
+
+            platform = std::shared_ptr<PlatformPlugin>(platformRegistry->pluginObject(plugin.name));
+        }
+
+        if (!platform) {
+            qCCritical(UNION_GENERAL) << "Could not find a supported platform plugin for platform" << platformName << "and session" << sessionName;
+            qCCritical(UNION_GENERAL) << "Creating a default fallback platform plugin";
+            platform = std::make_shared<FallbackPlatformPlugin>();
+        }
+    }
+
     std::shared_ptr<PluginRegistry<InputPlugin>> inputRegistry;
     QHash<QPair<QString, QString>, std::shared_ptr<Style>> styles;
+
+    std::shared_ptr<PluginRegistry<PlatformPlugin>> platformRegistry;
+    std::shared_ptr<PlatformPlugin> platform;
 };
 
 StyleRegistry::StyleRegistry(std::unique_ptr<StyleRegistryPrivate> &&d)
@@ -88,8 +150,19 @@ void StyleRegistry::save()
 
 std::shared_ptr<Style> StyleRegistry::defaultStyle()
 {
-    static auto name = qEnvironmentVariable("UNION_STYLE_NAME", u"breeze"_s);
-    static auto plugin = qEnvironmentVariable("UNION_STYLE_PLUGIN", u"union-input-css"_s);
+    static auto environmentPlugin = qEnvironmentVariable("UNION_STYLE_PLUGIN", QString{});
+    static auto environmentName = qEnvironmentVariable("UNION_STYLE_NAME", QString{});
+
+    auto plugin = environmentPlugin;
+    if (plugin.isEmpty()) {
+        plugin = platform()->defaultInputPlugin();
+    }
+
+    auto name = environmentName;
+    if (name.isEmpty()) {
+        name = platform()->defaultStyleName();
+    }
+
     return style(name, plugin);
 }
 
@@ -135,6 +208,15 @@ void Union::StyleRegistry::addStyle(const std::shared_ptr<Style> &style)
     d->styles.insert(styleId, style);
 }
 
+std::shared_ptr<PlatformPlugin> Union::StyleRegistry::platform() const
+{
+    if (!d->platform) {
+        d->loadPlatform();
+    }
+
+    return d->platform;
+}
+
 void StyleRegistry::cleanup()
 {
     auto instance = StyleRegistry::instance();
@@ -143,6 +225,9 @@ void StyleRegistry::cleanup()
 
     instance->d->styles.clear();
     instance->d->inputRegistry.reset();
+
+    instance->d->platform.reset();
+    instance->d->platformRegistry.reset();
 }
 
 std::shared_ptr<StyleRegistry> StyleRegistry::instance()
