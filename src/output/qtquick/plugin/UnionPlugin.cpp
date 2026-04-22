fb6ac2189920863649326b389fb9019fe897686b
Akseli Lahtinen <akselmo@akselmo.dev>
2026-04-22 16:47:38 +0300

    QuickStyle: Setup text rendering
    Use NativeTextRendering if not using fractional scaling,
    otherwise use QtTextRendering.
    
    This is a workaround for QTBUG-126577

--- src/output/qtquick/plugin/UnionPlugin.cpp
+++ src/output/qtquick/plugin/UnionPlugin.cpp
@@ -2,12 +2,22 @@
 // SPDX-FileCopyrightText: 2024 Arjen Hiemstra <ahiemstra@heimr.nl>
 
 #include "UnionPlugin.h"
-
+#include <QGuiApplication>
+#include <QQuickWindow>
 #include <StyleRegistry.h>
 
 UnionPlugin::UnionPlugin(QObject *parent)
     : QQmlEngineExtensionPlugin(parent)
 {
+    // NativeTextRendering is still distorted sometimes with fractional scale factors
+    // Given Qt disables all hinting with native rendering when any scaling is used anyway
+    // we can use Qt's rendering throughout
+    // QTBUG-126577
+    if (qApp->devicePixelRatio() == 1.0) {
+        QQuickWindow::setTextRenderType(QQuickWindow::NativeTextRendering);
+    } else {
+        QQuickWindow::setTextRenderType(QQuickWindow::QtTextRendering);
+    }
 }
 
 void UnionPlugin::initializeEngine(QQmlEngine *, const char *)
