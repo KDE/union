9f163df4af78799accc1c05e6d476e6ad7378ea4
Arjen Hiemstra <ahiemstra@heimr.nl>
2026-04-29 13:50:25 +0200

    tools/ruleinspector: Support declaring multiple elements through command line
    Use "--child" to indicate that a new element is started and that any
    following arguments belong to that child. Additionally add a "verbose"
    argument that sets query logging to full.

--- tools/ruleinspector/main.cpp
+++ tools/ruleinspector/main.cpp
@@ -16,6 +16,20 @@
 
 using namespace Qt::StringLiterals;
 
+struct ArgumentsData {
+    const QStringList types;
+    const QStringList ids;
+    const QStringList states;
+    const QStringList hints;
+    const QStringList attributes;
+
+    QStringList::const_iterator currentType = types.begin();
+    QStringList::const_iterator currentId = ids.begin();
+    QStringList::const_iterator currentState = states.begin();
+    QStringList::const_iterator currentHint = hints.begin();
+    QStringList::const_iterator currentAttribute = attributes.begin();
+};
+
 int main(int argc, char **argv)
 {
     QGuiApplication app(argc, argv);
@@ -26,16 +40,21 @@ int main(int argc, char **argv)
     parser.addVersionOption();
 
     parser.addOptions({
-        // {u"plugin"_s, u"The input plugin to use"_s},
-        {u"style"_s, u"The style to use"_s, u"style"_s},
+        {u"style"_s, u"The style to use. This needs to be specified before any other arguments."_s, u"style"_s},
+        {u"verbose"_s, u"Verbose logging, including full query matching logs."_s, u"verbose"_s},
         {u"type"_s, u"The type to match"_s, u"type"_s},
         {u"id"_s, u"The ID to match"_s, u"id"_s},
-        {u"states"_s, u"The states to match"_s, u"states"_s},
-        {u"hint"_s, u"The hint to match. Can be specified multiple times."_s, u"hint"_s},
-        {u"attribute"_s, u"An attribute and value to match, specified as \"key=value\". Can be specified multiple times."_s, u"attribute"_s},
+        {u"state"_s, u"The state to match. Can be specified multiple times per element."_s, u"state"_s},
+        {u"hint"_s, u"The hint to match. Can be specified multiple times per element."_s, u"hint"_s},
+        {u"attribute"_s, u"An attribute and value to match, specified as \"key=value\". Can be specified multiple times per element."_s, u"attribute"_s},
+        {u"child"_s, u"Indicates that the following arguments apply to a new element."_s},
     });
 
-    parser.process(app);
+    parser.process(app.arguments());
+
+    if (parser.isSet(u"verbose"_s)) {
+        QLoggingCategory::setFilterRules(u"union.query*=true"_s);
+    }
 
     auto registry = Union::StyleRegistry::instance();
     registry->load();
@@ -54,34 +73,58 @@ int main(int argc, char **argv)
 
     Union::ElementQuery query(style);
 
-    Union::Element::Ptr queryElement = Union::Element::create();
-    queryElement->setType(parser.value(u"type"_s));
-    queryElement->setId(parser.value(u"id"_s));
+    Union::ElementList elements;
+    Union::Element::Ptr currentElement = Union::Element::create();
+    Union::Element::States currentStates;
+    QStringList currentHints;
+    QVariantMap currentAttributes;
 
-    if (parser.isSet(u"states"_s)) {
-        auto statesEnum = queryElement->metaObject()->enumerator(queryElement->metaObject()->indexOfEnumerator("State"));
-        queryElement->setStates(static_cast<Union::Element::States>(statesEnum.keysToValue(parser.value(u"states"_s).toUtf8().data())));
-    }
+    auto appendElement = [&]() {
+        currentElement->setStates(currentStates);
+        currentElement->setHints(currentHints);
+        currentElement->setAttributes(currentAttributes);
+        elements.append(currentElement);
 
-    if (parser.isSet(u"hint"_s)) {
-        const auto hints = parser.values(u"hint"_s);
-        queryElement->setHints(QStringList(hints.begin(), hints.end()));
-    }
+        currentElement = Union::Element::create();
+        currentStates = Union::Element::States{};
+        currentHints = QStringList{};
+        currentAttributes = QVariantMap{};
+    };
 
-    if (parser.isSet(u"attribute"_s)) {
-        const auto attributes = parser.values(u"attribute"_s);
-        for (const auto &attribute : attributes) {
-            auto parts = attribute.split(u'=');
+    ArgumentsData data{
+        .types = parser.values(u"type"_s),
+        .ids = parser.values(u"id"_s),
+        .states = parser.values(u"state"_s),
+        .hints = parser.values(u"hint"_s),
+        .attributes = parser.values(u"attribute"_s),
+    };
+
+    for (auto argument : parser.optionNames()) {
+        if (argument == u"type") {
+            currentElement->setType(*data.currentType++);
+        } else if (argument == u"id") {
+            currentElement->setId(*data.currentId++);
+        } else if (argument == u"state") {
+            const auto statesEnum = QMetaEnum::fromType<Union::Element::States>();
+            auto value = static_cast<Union::Element::State>(statesEnum.keyToValue((*data.currentState++).toUtf8().data()));
+            currentStates &= value;
+        } else if (argument == u"hint") {
+            currentHints.append(*data.currentHint++);
+        } else if (argument == u"attribute") {
+            auto value = *data.currentAttribute++;
+            auto parts = value.split(u'=');
             if (parts.length() != 2) {
-                std::cout << "Ignoring invalid attribute" << attribute.toStdString();
-                continue;
+                std::cout << "Ignoring invalid attribute" << value.toStdString();
+            } else {
+                currentAttributes.insert(parts.at(0), parts.at(1));
             }
-
-            queryElement->setAttribute(parts.at(0), parts.at(1));
+        } else if (argument == u"child") {
+            appendElement();
         }
     }
+    appendElement();
 
-    query.setElements({queryElement});
+    query.setElements(elements);
     query.execute();
 
     if (!query.hasMatches() || !query.properties()) {
