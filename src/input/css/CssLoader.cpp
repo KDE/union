fd9997c86a15cb745850263d214e4511ea20a27c
Akseli Lahtinen <akselmo@akselmo.dev>
2026-06-09 13:03:39 +0300

    WordWrap: Fix enum parsing
    Wrap was never resolved to anything properly, so it just resolved into
    WrapAnywhere. This adds the "Wrap" property that will be manually handled to
    be resolved into WrapAtWordBoundaryOrAnywhere which is the expected behavior.
    
    BUG: 521210
    FIXED-IN: 6.7.0

--- src/input/css/CssLoader.cpp
+++ src/input/css/CssLoader.cpp
@@ -667,7 +667,13 @@ void CssLoader::setTextProperty(StyleProperty *output, const cssparser::Property
     }
 
     if (property.name() == "text-wrap-mode") {
-        text->setWrapMode(toEnumValue<TextWrapMode>(property.value<std::string>()));
+        const auto val = property.value<std::string>();
+        // Wrap is shorthand for WrapAtWordBoundaryOrAnywhere
+        if (val == "wrap") {
+            text->setWrapMode(TextWrapMode::WrapAtWordBoundaryOrAnywhere);
+        } else {
+            text->setWrapMode(toEnumValue<TextWrapMode>(property.value<std::string>()));
+        }
     }
 
     if (property.name() == "text-elide") {
