9eb5cd8c4154eb7a9b02bf7f3c88d2755b545e74
Arjen Hiemstra <ahiemstra@heimr.nl>
2026-08-31 16:53:28 +0200

    output/widgets: Silence warning about unused function in ElementCache
    Not sure why it's complaining, but marking it inline makes sense anyway
    as it's just a single call.

--- src/output/qtwidgets/ElementCache.h
+++ src/output/qtwidgets/ElementCache.h
@@ -63,7 +63,7 @@ static std::shared_ptr<AbstractElement> element(int enumValue, size_t hash, cons
     return result;
 }
 
-static void clear()
+inline static void clear()
 {
     detail::s_elementCache.clear();
 }
