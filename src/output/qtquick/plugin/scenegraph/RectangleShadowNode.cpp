b8f7df183f222b0d42a92ca16b760d3b66063abe
Arjen Hiemstra <ahiemstra@heimr.nl>
2026-09-15 11:48:46 +0200

    output/quick: Prevent divide by zero in StyledRectangle
    If one size ends up being 0 we end up with a minDimension of 0 which
    leads to divide by zero errors. Instead use at least a tiny value so we
    prevent that.

--- src/output/qtquick/plugin/scenegraph/RectangleShadowNode.cpp
+++ src/output/qtquick/plugin/scenegraph/RectangleShadowNode.cpp
@@ -90,14 +90,14 @@ void RectangleShadowNode::update()
     setRect(r);
 
     auto aspect = r.width() > r.height() ? QVector2D{float(r.width() / r.height()), 1.0} : QVector2D{1.0, float(r.height() / r.width())};
-    auto minDimension = std::min(r.width(), r.height());
+    auto minDimension = std::max(float(std::min(r.width(), r.height())), 0.0001f);
 
     UniformDataStream stream(uniformData());
     stream << UniformDataStream::Placeholder::ModelViewProjectionMatrix // matrix
            << UniformDataStream::Placeholder::Opacity // opacity
            << aspect // aspect
-           << float(m_spread / minDimension) // spread
-           << float(m_blur / minDimension) // blur
+           << m_spread / minDimension // spread
+           << m_blur / minDimension // blur
            << m_radius / minDimension // radius
            << ShaderNode::toPremultiplied(m_color); // color
 
